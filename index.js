eb16d89730470c291811d64f7f3b15f47221aaa7
ainurrahman <ainur.rahman@amartha.com>
2022-06-19 11:30:00 +0700

    feat(MAIN-000): add all feature

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-var $sidebarAndWrapper = $("#sidebar");
-$("#icon-clickable").click(function () {
-    $sidebarAndWrapper.toggleClass("hide-sidebar");
+let sidebarAndWrapper = $("#sidebar");
+
+$(window).on('resize', function(){
+    var win = $(this); //this = window
+    if (win.width() < 600) {
+      $("#icon-clickable").click(function () {
+        sidebarAndWrapper.toggleClass("hide-sidebar");
+      });
+    }else{
+      $("#icon-clickable").click(function () {
+        sidebarAndWrapper.toggleClass("minimize-sidebar");
+      });
+    }
 });
