5c93c30f04e7308ac6098823d2c42ff1aebecab3
ainurrahman <ainur.rahman@amartha.com>
2022-06-19 11:40:23 +0700

    feat(MAIN-000): remove unused variable

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ $(window).on('resize', function(){
       });
     }
     if (win.width() >= 600){
-      console.log("masuk sini maze 610")
       $("#icon-clickable").click(function () {
         sidebarAndWrapper.toggleClass("minimize-sidebar");
       });
