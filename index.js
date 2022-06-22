c7bba3804957f8c65c428e011bf172e22c3ce3fe
AInur Rahman <ainur.rahman@amartha.com>
2022-06-22 14:19:43 +0700

    feat(MAIN-000): adjustment code

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-let sidebarAndWrapper = $("#sidebar");
-
-$(window).on('resize', function(){
-    var win = $(this); //this = window
-    if (win.width() < 600) {
+function check() {
+    let sidebarAndWrapper = $("#sidebar");
+    if ($(document).width() < 600) { // use `===` and no quote around 783
       $("#icon-clickable").click(function () {
         sidebarAndWrapper.toggleClass("hide-sidebar");
       });
     }
-    if (win.width() >= 600){
+
+    if ($(document).width() >= 600) { // use `===` and no quote around 783
       $("#icon-clickable").click(function () {
         sidebarAndWrapper.toggleClass("minimize-sidebar");
       });
     }
+}
+
+$(document).ready(function() {
+  check();
+});
+
+$(window).resize(function () {  // no `on` here
+  check();
 });
